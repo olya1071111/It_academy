3a5744e893dcf8d8f5fbfb1d8bc56985c130cb40
olya1071111 <104586687+olya1071111@users.noreply.github.com>
2023-08-24 19:51:54 +0300

    Add files via upload

---
+++ tasks3_4/src/com/itacademy/home13/task3/Group.java
@@ -0,0 +1,32 @@
+package com.itacademy.home13.task3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Group {
+
+	private List<Student> stud;
+
+	public Group() {
+		stud = new ArrayList<Student>();
+	}
+
+	public boolean add(Student b) {
+		return stud.add(b);
+	}
+
+	public void bestStudent() {
+		for (int i = 0; i < stud.size(); i++) {
+			Student b = stud.get(i);
+			int fl = 0;
+			for (int a : b.getMas()) {
+				if (a < 9) {
+					fl++;
+				}
+			}
+			if (fl == 0)
+				System.out.println("№ группы - " + b.getNumGroup() + "\t " + b.getName());
+		}
+	}
+
+}
